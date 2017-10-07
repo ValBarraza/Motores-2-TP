9f3d381e90455cf8862395a24f4cbb20702939fb
Lucas <lucas.incerti@davinci.edu.ar>
2017-10-07 16:40:22 -0300

    proyecto base
    Aca puede empezar a editar, guarden y suban sus propios scripts del hud que le tocó hacer. Depsues los junto

---
+++ TP2-Motores II/Assets/Editor/HUD Generator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class HUDGenerator : EditorWindow {
+
+    [MenuItem("HUD/HUD Generator")]
+    static void openWindow()
+    {
+        GetWindow<HUDGenerator>();
+    }
+
+    private void OnGUI()
+    {
+
+    }
+}
