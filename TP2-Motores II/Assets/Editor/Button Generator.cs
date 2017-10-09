425ca46eb32e29d25647b0da2345566fdc0256d5
Ayusmi <ayelen.micale@davinci.edu.ar>
2017-10-08 23:19:32 -0300

    Botones para todos y todas
    Un lio GitHub, ya no se q hago! A veerrr

---
+++ TP2-Motores II/Assets/Editor/Button Generator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class ButtonGenerator : EditorWindow
+{
+    public Button buttonObject;
+    public Font fontObject;
+    //public Text buttonText;
+    public string buttonText;
+    public Vector3 buttonPos;
+    public int fontSize;
+
+    [MenuItem("HUD/Buttons Generator")]
+    static void CreateWindow()
+    {
+        ((ButtonGenerator)GetWindow(typeof(ButtonGenerator))).Show();
+    }
+
+    void OnEnable()
+    {
+        //GetComponent<Text>();
+    }
+
+
+    private void OnGUI()
+    {
+        EditorGUILayout.Space();
+        buttonObject = (Button)EditorGUILayout.ObjectField("Button: ", buttonObject, typeof(Button), false);
+        buttonText = EditorGUILayout.TextField("Text: ", buttonText);
+        fontObject = (Font)EditorGUILayout.ObjectField("Font: ", fontObject, typeof(Font), false);
+        fontSize = EditorGUILayout.IntField("Text Size: ", fontSize);
+        buttonPos = EditorGUILayout.Vector3Field("Position: ", buttonPos);
+    }
+
+
+}
