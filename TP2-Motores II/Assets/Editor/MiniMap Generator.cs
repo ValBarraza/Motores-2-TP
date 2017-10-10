be5ad292c51013dfd0b5206ff9b9116c63220a55
Lucas <lucas.incerti@davinci.edu.ar>
2017-10-10 10:13:12 -0300

    miniMap
    se agregó un scrip basico de minimap

---
+++ TP2-Motores II/Assets/Editor/MiniMap Generator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MiniMapGenerator : EditorWindow {
+
+
+    private Camera _cam;
+    private RenderTexture _map;
+    private float _high;
+
+    [MenuItem("HUD/miniMap")]
+    static void OpenWindow()
+    {
+        GetWindow<MiniMapGenerator>();
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.ObjectField("view(camera)", _cam, typeof(Camera), true);
+        EditorGUILayout.ObjectField("view(minimap in scene)", _cam, typeof(RenderTexture), true);
+    }
+}
