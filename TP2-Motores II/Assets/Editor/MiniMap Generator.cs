8170db8256d0b8e9695ffa90d0ddf5bf182d8077
Lucas <lucas.incerti@davinci.edu.ar>
2017-10-17 10:13:11 -0300

    MiniMap/TextGenerator
    Se ordenó el codigo del miniMap
    En el textGenerator se puede hacer textos estaticos con un boton.

--- TP2-Motores II/Assets/Editor/MiniMap Generator.cs
+++ TP2-Motores II/Assets/Editor/MiniMap Generator.cs
@@ -6,16 +6,34 @@ using UnityEngine.UI;
 
 public class MiniMapGenerator : EditorWindow {
 
-    
-    private Camera _cam;
-    private RenderTexture _map;
-    private Sprite _imageTexture;
-    private float _high;
-
-
-    private GameObject cam;
+    //CANVAS
+    private Canvas canvasMain;
     private GameObject _canvas;
-    private GameObject texture;
+
+    //CAM
+    private Camera _cam;
+    private GameObject cam;
+    private int _highCam;
+    private float fow;
+
+    
+
+
+
+    //TEXTURE
+    private RenderTexture _RTexture;
+    private int _highRenderTexture;
+    private int _widthRenderTexture;
+
+    //MAP
+    private GameObject _imageCont;
+    private int _highMap;
+    private int _widthMap;
+
+
+
+   
+   
 
 
 
@@ -25,53 +43,130 @@ public class MiniMapGenerator : EditorWindow {
         GetWindow<MiniMapGenerator>();
     }
 
+    private void OnEnable()
+    {
+        if (canvasMain == null)
+        {
+            canvasMain = FindObjectOfType<Canvas>();
+
+        }
+    }
+
     private void OnGUI()
     {
         _cam = (Camera)EditorGUILayout.ObjectField("view(camera)", _cam, typeof(Camera), true);
-        _map = (RenderTexture)EditorGUILayout.ObjectField("view(minimap in scene)", _map, typeof(RenderTexture), true);
+        _RTexture = (RenderTexture)EditorGUILayout.ObjectField("view(minimap in scene)", _RTexture, typeof(RenderTexture), true);
 
-        if (GUILayout.Button("add canvas"))
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField(" size texture ");
+        _highRenderTexture = EditorGUILayout.IntField(_highRenderTexture);
+        EditorGUILayout.LabelField(" x ");
+        _widthRenderTexture = EditorGUILayout.IntField(_widthRenderTexture);
+        EditorGUILayout.EndHorizontal();
+        if (_highRenderTexture <= 0 || _widthRenderTexture <= 0)
+        {
+            EditorGUILayout.HelpBox("sizes of the texture render can't be 0", MessageType.Error);
+        }
+
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField(" size map ");
+        _highMap = EditorGUILayout.IntField(_highMap);
+        EditorGUILayout.LabelField(" x ");
+        _widthMap = EditorGUILayout.IntField(_widthMap);
+        EditorGUILayout.EndHorizontal();
+        if (_highMap <= 0 || _widthMap <= 0)
+        {
+            EditorGUILayout.HelpBox("sizes of the map render can't be 0", MessageType.Error);
+        }
+
+
+        EditorGUILayout.LabelField(" high camera ");
+        _highCam = EditorGUILayout.IntField(_highCam);
+        // _cam.GetComponent<Camera>().fieldOfView = fow;
+        _cam.transform.position = new Vector3(0, _highCam, 0);
+        if (GUILayout.Button("add camera and MiniMap"))
+        {
+            SetOfCam();
+            SetOfMiniMap();
+        }
+
+        SetCanvas();
+
+
+
+        if (GUILayout.Button("add Render Texture"))
+        {
+            SetRenderTexture();
+        }
+
+      
+
+    }
+
+
+//CONFIGURACION RENDER TEXTURE
+    void SetRenderTexture()
+    {
+        if (_highRenderTexture > 0 || _widthRenderTexture > 0)
+        {
+            RenderTexture renderTexture = new RenderTexture(_highRenderTexture, _widthRenderTexture, 1);
+            AssetDatabase.CreateAsset(renderTexture, "Assets/MyTextureNew.renderTexture");
+            _imageCont.GetComponent<RawImage>().texture = renderTexture;
+            cam.GetComponent<Camera>().targetTexture = renderTexture;
+            _RTexture = renderTexture;
+        }
+    }
+
+ //CONFIGURACION DEL CANVAS
+    void SetCanvas()
+    {
+        if (GUILayout.Button("add canvas") && FindObjectOfType<Canvas>() == null)
         {
             _canvas = new GameObject();
             _canvas.AddComponent<RectTransform>();
             _canvas.AddComponent<Canvas>();
             _canvas.AddComponent<CanvasScaler>();
             _canvas.AddComponent<GraphicRaycaster>();
-            _canvas.transform.position = new Vector3(227.5f,128f, _canvas.transform.position.z);
-            _canvas.GetComponent<RectTransform>().sizeDelta = new Vector2(455,256);
+            _canvas.transform.position = new Vector3(227.5f, 128f, _canvas.transform.position.z);
+            _canvas.GetComponent<RectTransform>().sizeDelta = new Vector2(455, 256);
+            canvasMain = _canvas.AddComponent<Canvas>();
             _canvas.name = "Main Canvas";
-
         }
-
-
-        if (GUILayout.Button("add camera and MiniMap"))
+        else if (FindObjectOfType<Canvas>() != null)
         {
-            cam = new GameObject();
-            texture = new GameObject();
-            texture.AddComponent<RectTransform>();
-            texture.AddComponent<CanvasRenderer>();
-            texture.AddComponent<RawImage>();
-            cam.AddComponent<Camera>();
-            cam.transform.SetParent(_canvas.transform);
-            texture.transform.SetParent(_canvas.transform);
-            cam.name = "Cam";
-            texture.name = "miniMap";
+
+            EditorGUILayout.HelpBox("there's a canvas created", MessageType.Info);
         }
-        //
+    }
 
+ //CONFIGURACION DE LA CAM
+   void SetOfCam()
+    {
+        cam = new GameObject();
+        cam.AddComponent<Camera>();
+        // cam.transform.SetParent(canvasMain.transform);
+        _cam = cam.GetComponent<Camera>();
+        cam.transform.Rotate(90, 0, 0);
+        cam.transform.position = new Vector3(0, 1500f, 0);
+        cam.name = "Cam";
 
-        if (GUILayout.Button("add Render Texture"))
-        {
-            RenderTexture renderTexture = new RenderTexture(100,100,1);
-          
-            AssetDatabase.CreateAsset(renderTexture, "Assets/MyTextureNew.renderTexture");
-             //= _map;
+    }
 
-            texture.GetComponent<RawImage>().texture = renderTexture;
-            cam.GetComponent<Camera>().targetTexture = renderTexture;
-        }
-        //cam.GetComponent<Camera>().targetTexture = _map;
-
-
+//CONFIGURACION DEL MINI MAP
+   void SetOfMiniMap()
+    {
+       
+        
+            
+            _imageCont = new GameObject();
+            _imageCont.AddComponent<RectTransform>();
+            _imageCont.AddComponent<CanvasRenderer>();
+            _imageCont.AddComponent<RawImage>();
+            _imageCont.transform.SetParent(canvasMain.transform);
+            _imageCont.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _widthMap);
+            _imageCont.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _highMap);
+            _imageCont.name = "miniMap";
+        
     }
 }
--- TP2-Motores II/Assets/Editor/TextGenerator.cs
+++ TP2-Motores II/Assets/Editor/TextGenerator.cs
@@ -4,40 +4,115 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
 
+//Que pueda crear un texto en el canvas y que pueda personalizarlo lo más posible desde la ventana
+//BACKGROUND DE TEXTO //MARCO ------- 
+
 public class TextGenerator : EditorWindow
 {
-    public string _text;
-    public Sprite image;
+    private Text _textToGenerateStatic;
     private Font _font;
+    private Sprite _representationImage;
+    private Sprite _background;
+    private string _text;
+    bool _showStaticPersonalization;
+
     private int _objectsCount;
     private Text _dinamicText;
+
+
+    private GameObject _textObjetc;
+
+    Canvas _canvas;
 
     [MenuItem("HUD/Text Generator")]
     static void CreateWindow()
     {
         ((TextGenerator)GetWindow(typeof(TextGenerator))).Show();
     }
+
+    private void OnEnable()
+    {
+        if(_canvas == null)
+        _canvas = FindObjectOfType<Canvas>();
+    }
+
     private void OnGUI()
     {
         Repaint();
 
         StaticText();
-
-        EditorGUILayout.Space();
-
-        DinamicText();
-        
+        DinamicText();       
     }
+    #region Static
     void StaticText() {
         EditorGUILayout.LabelField("Textos Estáticos", EditorStyles.boldLabel);
 
+        //Esto--------------
+        _canvas = (Canvas)EditorGUILayout.ObjectField("Canvas", _canvas, typeof(Canvas), true);
+        _textToGenerateStatic = (Text)EditorGUILayout.ObjectField("aay dios", _textToGenerateStatic, typeof(Text), true);
+        //------------------
+
         _font = (Font)EditorGUILayout.ObjectField("Fuente", _font, typeof(Font), true);
-        _text = EditorGUILayout.TextField("Texto(texto)", _text);
-        if (_text != null) EditorGUILayout.HelpBox("Hay algo", MessageType.Info);
-        else EditorGUILayout.HelpBox("no hay nada", MessageType.Info);
-        image = (Sprite)EditorGUILayout.ObjectField("Representación", image, typeof(Sprite), true);
+        _text = EditorGUILayout.TextField("Texto(texto)", _text);        
+
+        _showStaticPersonalization = EditorGUILayout.Foldout(_showStaticPersonalization, "Personalizacion (opcional)");
+        if (_showStaticPersonalization)
+        {
+            StaticPersonalization();
+        }
+
+
+
+      
+
+
+            if (GUILayout.Button("Generar Texto")) {
+
+           // GenerateStaticText();
+
+                if (_font != null || _text != null)
+                 {
+                GenerateStaticText(); //si funciona descomentar esto
+                 }
+               
+        }
+        else if(_font == null)
+            EditorGUILayout.HelpBox("Debes agregar una fuente/font", MessageType.Error);
     }
-    void DinamicText() { 
+    void StaticPersonalization() {
+        _representationImage = (Sprite)EditorGUILayout.ObjectField("Representación", _representationImage, typeof(Sprite), true);
+        _background = (Sprite)EditorGUILayout.ObjectField("Background de texto", _background, typeof(Sprite), true);
+        //Marco
+    }
+    void GenerateStaticText() {
+        //Creo un GameObjetc en la escena
+        _textObjetc = new GameObject();
+
+        //Le agrego los componentes de TEXT
+        _textObjetc.AddComponent<RectTransform>();
+        _textObjetc.AddComponent<CanvasRenderer>();
+        _textObjetc.AddComponent<Text>();
+
+        //Le seteo el padre
+        _textObjetc.transform.SetParent(_canvas.transform);
+
+        //Lo igualo a tus variables
+        _textToGenerateStatic = _textObjetc.GetComponent<Text>();
+        _textToGenerateStatic.font = _font;
+        _textToGenerateStatic.text = _text;
+        _textObjetc.name = _text + " (static text)";
+
+
+        //Text txt = Instantiate(_textToGenerateStatic);
+        //_textToGenerateStatic.transform.SetParent(_canvas.transform);
+    }
+
+
+
+
+    #endregion
+    #region Dinamics
+    void DinamicText() {
         //Como hacer que el texto se adapte al tamaño de la ventana?
         //Como vincular un contador creado por el usuario? 
 
@@ -48,5 +123,6 @@ public class TextGenerator : EditorWindow
         _dinamicText = (Text)EditorGUILayout.ObjectField("Texto(UI)", _dinamicText, typeof(Text), true);
 
         //_dinamicText.text = "" + _objectsCount;
-    } 
+    }
+    #endregion
 }
