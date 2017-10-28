3dd8ed28af3b93274a877eb664fc639ea1273fa1
ValBarraza <valentina.barraza@davinci.edu.ar>
2017-10-28 18:58:34 -0300

    Prueba de error

--- TP2-Motores II/Assets/Editor/TextElementConfig.cs
+++ TP2-Motores II/Assets/Editor/TextElementConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
-
+//Configuración del elemento
 public class TextElementConfig : EditorWindow {
 
     private GameObject _element;
