e62fb80e2c1c2c0ae7cd4095d3e62c397955643b
ValBarraza <valentina.barraza@davinci.edu.ar>
2017-10-28 23:00:41 -0300

    Configuracion de texto
    Sin errores- creo.

--- TP2-Motores II/Assets/Editor/TextElementConfig.cs
+++ TP2-Motores II/Assets/Editor/TextElementConfig.cs
@@ -8,38 +8,55 @@ public class TextElementConfig : EditorWindow {
 
     private GameObject _element;
 
+    private Text _componentText;
+    private RectTransform _componentRectTrans;
+
     private Vector2 _ofMin;
     private Font _font;
     private string _text;
     private int _size;
 
-    public static void CreatedWindow()
-    {
+    public static void CreatedWindow() {
         GetWindow<TextElementConfig>();
     }
+    private void OnEnable()
+    {
+        Debug.Log("Inicio");
+        if(_element != null) GetFieldsOfElement();
+
+    }
     private void OnGUI()
+    {
+        Fields();
+        if (GUILayout.Button("Realizar Cambios")) MakeChanges();
+    }
+    void Fields()
     {
         _font = (Font)EditorGUILayout.ObjectField("Fuente", _font, typeof(Font), true);
         _size = EditorGUILayout.IntField("Tamaño del texto", _size);
         _text = EditorGUILayout.TextField("Texto(texto)", _text);
         _ofMin = EditorGUILayout.Vector2Field("OfsetMin", _ofMin);
-
-        if(GUILayout.Button("Realizar Cambios")) {
-            Config();
-        }
     }
+    void GetFieldsOfElement()
+    {
+        _componentText = _element.GetComponent<Text>();
+        _text = _componentText.text;
+        _font = _componentText.font;
+        _size = _componentText.fontSize;
 
-    public void Config()
-     {
-        var component = _element.GetComponent<Text>();
-        component.text = _text;
-        component.font = _font;
-        component.fontSize = _size;
+        _componentRectTrans = _element.GetComponent<RectTransform>();
+        _ofMin = _componentRectTrans.offsetMin;
+    }
+    void MakeChanges()
+    {
+        _componentText.text = _text;
+        _componentText.font = _font;
+        _componentText.fontSize = _size;
 
-        var pos = _element.GetComponent<RectTransform>();
-        pos.offsetMin = _ofMin;
-     }    
-    public void TextElementConfigMethod(GameObject element) {
+        _componentRectTrans.offsetMin = _ofMin;
+    }    
+    public void TextElementConfigMethod(GameObject element)
+    {
         _element = element;
     } 
 }
